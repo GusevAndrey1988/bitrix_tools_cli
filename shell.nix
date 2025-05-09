55060b8c51b6bd9c63dde4ce2ee727782e8cc503
Gusev Andrey <gusevandrey1988@gmail.com>
2025-05-09 23:40:20 +0300

    upd: CMakeList

--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
 let
-    pkgs = import <nixpkgs> {};
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz";
+    pkgs = import nixpkgs {};
 in
 pkgs.mkShell {
-
+    buildInputs = with pkgs; [
+        boost186
+        pkg-config
+    ];
 }
