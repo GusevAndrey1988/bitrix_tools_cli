e227f5e8138933b5928b32c1f866a1cfb1d342fe
Gusev Andrey <gusevandrey1988@gmail.com>
2025-05-10 10:52:40 +0300

    upd: удачная сборка

--- shell.nix
+++ shell.nix
@@ -1,11 +1,17 @@
 let
     nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz";
     pkgs = import nixpkgs {};
+    lib = pkgs.lib;
 in
 pkgs.mkShell {
     buildInputs = with pkgs; [
+        boost186
     ];
 
     nativeBuildInputs = with pkgs; [
+        pkg-config
     ];
+
+    BOOST_INCLUDEDIR = "${lib.getDev pkgs.boost186}/include";
+    BOOST_LIBRARYDIR = "${lib.getLib pkgs.boost186}/lib";
 }
