6d1472ae3625ff026fb696c8868abbc58f5aa134
Gusev Andrey <gusevandrey1988@gmail.com>
2025-05-11 17:22:02 +0300

    add: добавлена поддержка тестов

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 let
     nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz";
     pkgs = import nixpkgs {};
-    lib = pkgs.lib;
 in
 pkgs.mkShell {
     buildInputs = with pkgs; [
+        gtest
     ];
 
     nativeBuildInputs = with pkgs; [
---
+++ test/config_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(ConfigTest, BasicTest)
+{
+    EXPECT_EQ(41, 42);
+    EXPECT_EQ(24, 24);
+}
